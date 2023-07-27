e208eee0a26dd3c53bded2533bd9c20619bc92c8
mtobal93 <mtobal93@gmail.com>
2023-07-26 20:54:50 -0400

    phase 1 done

--- fetch-requests.js
+++ fetch-requests.js
@@ -7,13 +7,17 @@ Paste your code for fetch requests here once you finish each phase.
 
 // Your code here
 
-
+fetch("/products", {
+  method: "POST",
+  body: "name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery",
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded",
+  },
+});
 
 /* ============================== Phase 2 ============================== */
 
 // Your code here
-
-
 
 /* ============================== Phase 3 ============================== */
 
